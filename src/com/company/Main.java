6fe1685488aba63c57c30c4489c30cf480e02b04
Earn1ll <coldsteelblr@gmail.com>
2022-03-05 18:42:56 +0300

    less 3 abstract class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,13 @@ import less20.Music;
 import less21.Birds;
 import less21.Crown;
 import less21.Ostrich;
+import less23.AK47;
+import less23.Weapon;
 
 public class Main {
 
     public static void main(String[] args) {
-        Ostrich ostrich = new Ostrich();
-        Crown crown = new Crown();
-        ostrich.setName("Ostrich");
-        ostrich.walk();
-        ostrich.walk("Hi!");
+        Weapon weapon = new Weapon();
+        AK47 ak47 = new AK47();
     }
 }
---
+++ src/less23/Weapon.java
@@ -0,0 +1,10 @@
+package less23;
+
+public abstract class Weapon {
+    private String name;
+
+    public abstract void foo();
+    public void show(){
+        System.out.println("");
+    }
+}
